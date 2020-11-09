6278b38b2dfbdc69287d639af10b88de61172ff1
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-09 03:16:42 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,17 +1,9 @@
-# Use this space to code along with the readme
 def greeting
   puts "Hello World"
 end
 
-puts greeting
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+greeting
+greeting
+greeting
+greeting
+greeting
